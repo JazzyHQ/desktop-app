4f1854052631691abf73e0d31c1e3fa3fa584e1d
numan <numan856@gmail.com>
2023-10-31 20:27:07 -0700

    feat: Add nav menu to get back to the apps page. Also add frame to the setting window

--- src/main/main-manager.ts
+++ src/main/main-manager.ts
@@ -160,7 +160,6 @@ export default class MainProcessManager {
   public async createMainWindow() {
     this.mainWindow = new BrowserWindow({
       show: false,
-      frame: false,
       width: 800,
       height: 600,
       icon: MainProcessManager.getAssetPath('icon.png'),
